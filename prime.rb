9bb6d4ea92241fb65d55d875953ae6b2213dd201
Tara Ellicott <taraellicott@gmail.com>
2018-04-20 05:07:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+ def prime?(int)
+    if int < 2 
+     FALSE
+    elsif int == 2 
+       TRUE
+    else
+    i = 1 
+    arr = []
+      while i <= int && int > 2
+        if int % i == 0
+        arr << i
+        i += 1
+        end
+    if arr.length < 2 
+      TRUE
+    end
+     FALSE
+   end
+ end
+end
+ 
+  
